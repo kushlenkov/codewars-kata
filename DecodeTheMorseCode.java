d374979ee2ade8d77ec6971297e39ad92ce81a1a
kushlenkov <kushlenkov.av@gmail.com>
2024-03-11 13:39:38 +0300

    11.03.2024 13:37

--- DecodeTheMorseCode.java
+++ DecodeTheMorseCode.java
@@ -1,15 +1,15 @@
-//public class MorseCodeDecoder {
-//    public static String decode(String morseCode) {
-//        morseCode = morseCode.trim();
-//        String[] words = morseCode.split(" ");
-//        StringBuilder build = new StringBuilder();
-//        for (int i = 0; i < words.length; i++) {
-//            if (words[i].equals("") && !words[i+1].equals("")) {
-//                build.append(" ");
-//            } else if (!words[i].equals("")) {
-//                build.append(MorseCode.get(words[i])); // библиотека CW
-//            }
-//        }
-//        return build.toString();
-//    }
-//}
+public class MorseCodeDecoder {
+    public static String decode(String morseCode) {
+        morseCode = morseCode.trim();
+        String[] words = morseCode.split(" ");
+        StringBuilder build = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals("") && !words[i+1].equals("")) {
+                build.append(" ");
+            } else if (!words[i].equals("")) {
+                build.append(MorseCode.get(words[i])); // библиотека CW
+            }
+        }
+        return build.toString();
+    }
+}
